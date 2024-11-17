325dd6e3bcba6f98ab868b9d06efaa0a873bfc4a
Lucas <lupsilva@simpress.com.br>
2024-11-17 14:14:41 -0300

    refactor: migrando da startup para program

--- Program.cs
+++ Program.cs
@@ -1,19 +1,114 @@
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ReflectionIT.Mvc.Paging;
+using sh_rt;
+using sh_rt.Areas.Admin.Interfaces;
+using sh_rt.Areas.Admin.Services;
+using sh_rt.Context;
+using sh_rt.Interfaces;
+using sh_rt.Mappings;
+using sh_rt.Models;
+using sh_rt.Repositories;
+using sh_rt.Services;
+using sh_rt.Services.Interfaces;
 
-namespace sh_rt;
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(opts =>
+            opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+            );
+
+builder.Services.Configure<ImagesConfiguration>(builder.Configuration.GetSection("ImagesFolderConfiguration"));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddScoped(s => Cart.GetCart(s));
+
+builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
+builder.Services.AddTransient<IShirtRepository, ShirtRepository>();
+builder.Services.AddTransient<IOrderRepository, OrderRepository>();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+builder.Services.AddScoped<ISalesReportService, SalesReportService>();
+builder.Services.AddScoped<ISalesChartService, SalesChartService>();
+
+builder.Services.AddAuthorization(opts =>
 {
-    public static void Main(string[] args)
+    opts.AddPolicy("Admin", policy =>
     {
-        CreateHostBuilder(args)
-           .Build()
-           .Run();
-    }
+        policy.RequireRole("Admin");
+    });
+});
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+builder.Services.AddAutoMapper(typeof(OrderProfile));
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddPaging(opts =>
+{
+    opts.ViewName = "Bootstrap4";
+    opts.PageParameterName = "pageindex";
+});
+
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseSession();
+
+app.UseRouting();
+
+CreateProfiles(app);
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "filter",
+        pattern: "shirt/{action}/{category?}",
+        defaults: new { Controller = "Shirt", action = "GetShirts" }
+        );
+
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}"
+    );
+
+
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+
+
+app.Run();
+
+void CreateProfiles(WebApplication app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<ISeedUserRoleInitial>();
+        service.SeedUsers();
+        service.SeedRoles();
+    }
 }
